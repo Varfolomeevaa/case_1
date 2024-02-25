0c0ff57d365006d7ac56812371e131bf47f38419
Varfolomeevaa <v.varfolomeeva@g.nsu.ru>
2024-02-25 15:04:07 +0700

    2

--- main.py
+++ main.py
@@ -1,3 +1,14 @@
-n = int(input())
+import random
+stations = {}
+with open('gas_station.txt') as gas:
+    for x in gas:
+        number, turn, gs = x.split()
+        stations[number] = (turn, gs)
+cars = {}
+with open('input.txt') as f_inp:
+    for x in f_inp:
+        time, liter, gs = x.split()
+        cars[time] = (liter, gs)
 
-print('hello vika')
+for i in range(24):
+    for j in range(60):
